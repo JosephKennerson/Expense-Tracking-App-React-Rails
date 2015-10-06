5f004eaf6eb43cd9a717e066e87fce3c03244f7f
JosephKennerson <JosephMKennerson@gmail.com>
2015-10-05 19:14:59 -0700

    Integrating Bootstrap, React.js, Initializing 'Record' resource, creating Record and Records components and Display records"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
